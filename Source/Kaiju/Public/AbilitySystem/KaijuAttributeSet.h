3ebe39eac68496c9c12dc0e479dbe8d592624059
Wenhi <810319935@qq.com>
2025-12-21 23:54:47 +0800

    feat: 添加敌人的模型，添加ABP_Enemy,并且给敌人最基础的动画（BS_IdleWalkRun）

--- Source/Kaiju/Public/AbilitySystem/KaijuAttributeSet.h
+++ Source/Kaiju/Public/AbilitySystem/KaijuAttributeSet.h
@@ -6,6 +6,13 @@
 #include "AttributeSet.h"
 #include "KaijuAttributeSet.generated.h"
 
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 /**
  * 
  */
@@ -13,5 +20,6 @@ UCLASS()
 class KAIJU_API UKaijuAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
 	
 };
